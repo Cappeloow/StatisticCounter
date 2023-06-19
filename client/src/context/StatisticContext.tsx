40873c8b142eb134e99ffeeaf2a5650a7b3b6652
cappeloow <cappe1996@hotmail.com>
2023-06-19 21:48:11 +0200

    can now add one if click the button, but also the time stamp when added one is collected

--- client/src/context/StatisticContext.tsx
+++ client/src/context/StatisticContext.tsx
@@ -6,30 +6,51 @@ import {
   useContext,
 } from "react";
 import data from "../data.json";
-
+import { useLocalStorage } from "../hooks/localstorage";
 export interface IMatter {
   id: number;
   task: string;
+  time: string[];
+  amount: number;
   category: string;
 }
 
 export interface StatisticContext {
   matters: IMatter[];
   getStatistics: () => void;
+  addAmountToMatter: (matter: IMatter) => void;
 }
 
 const StatisticContext = createContext<StatisticContext>({
   matters: [],
   getStatistics: () => {},
+  addAmountToMatter: () => {},
 });
 
 export const useStatisticContext = () => useContext(StatisticContext);
 
 const StatisticProvider = ({ children }: PropsWithChildren) => {
-  const [matters, setMatter] = useState<IMatter[]>([]);
+  const [matters, setMatters] = useLocalStorage("cart", []);
+
+  const addAmountToMatter = (matter: IMatter) => {
+    const currentItem = matters.find((item: IMatter) => item.id === matter.id);
+    const currentDate = new Date();
+    const options = { timeZone: "Europe/Stockholm" };
+    const swedishTime = currentDate.toLocaleString("sv-SE", options);
+
+    console.log(swedishTime);
+    //kanske vill lägga specifika tider i arrayer?
+    // för att sedan lägga in arrayer i en egen kub för att redovisa antalet i length?
+
+    if (currentItem) {
+      currentItem.amount++;
+      currentItem.time.push(swedishTime);
+      setMatters([...matters]);
+    }
+  };
 
   async function getStatistics() {
-    setMatter(data);
+    setMatters(data);
   }
 
   useEffect(() => {
@@ -42,6 +63,7 @@ const StatisticProvider = ({ children }: PropsWithChildren) => {
         value={{
           matters,
           getStatistics,
+          addAmountToMatter,
         }}
       >
         {children}
