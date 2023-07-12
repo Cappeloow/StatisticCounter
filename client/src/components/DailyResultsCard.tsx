2eb9d05650929348e65c2fa540b0183923e87d38
cappeloow <cappe1996@hotmail.com>
2023-07-12 18:40:16 +0200

    got the timestamps from the statistics.lenght

--- client/src/components/DailyResultsCard.tsx
+++ client/src/components/DailyResultsCard.tsx
@@ -6,15 +6,25 @@ function DailyResultsCard() {
   const { dailyResults, matters } = useStatisticContext();
   const { Title } = Typography;
   const result = dailyResults(matters);
-  const [ten, setTen] = useState([]);
+  const [nine, setNine] = useState([]);
   const [eight, setEight] = useState([]);
+  const [twelve, setTwelve] = useState([]);
+  const [thirteen, setThirteen] = useState([]);
 
   function CalculateArrayOfTimes() {
     const allTimes = matters.flatMap((matter) => matter.time);
-    const timesEqualTo22 = allTimes.filter((time) => time >= 22 && time <= 23);
+    const timesBetween9And12 = allTimes.filter(
+      (time) => time >= 9 && time <= 12
+    );
+    const timesBetween13And16 = allTimes.filter(
+      (time) => time >= 13 && time <= 16
+    );
     const timesEqualTo8 = allTimes.filter((time) => time === 8);
-    setTen(timesEqualTo22);
+    const timesEqualTo12 = allTimes.filter((time) => time === 12);
     setEight(timesEqualTo8);
+    setNine(timesBetween9And12);
+    setTwelve(timesEqualTo12);
+    setThirteen(timesBetween13And16);
   }
 
   useEffect(() => {
@@ -24,8 +34,10 @@ function DailyResultsCard() {
   return (
     <div>
       <div className="Eight">
-        <p> 22.00-23.00: {ten.length}</p>
-        <p>8.00-9.00: {eight.length}</p>
+        <p>09.00-12.00: {nine.length}</p>
+        <p>08.00-09.00: {eight.length}</p>
+        <p>12.00-13.00: {twelve.length}</p>
+        <p>13.00-16.00: {thirteen.length}</p>
       </div>
       <Title level={4}>Totalt för dagen: {result}</Title>
     </div>
