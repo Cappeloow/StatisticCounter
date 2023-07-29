b39894b1912b034ca6ccf18ff3e8f650f16dc53a
cappeloow <cappe1996@hotmail.com>
2023-07-29 11:33:24 +0200

    hey

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Build the React project
-npm run build
+# Remove any existing gh-pages branch
+git branch -D gh-pages
 
 # Create a new gh-pages branch and check it out
 git checkout -b gh-pages
 
-# Remove everything in the branch except the built files from the dist folder
+# Remove everything in the branch except the built files from the client/build folder
 git rm -r .
-git restore --source=HEAD --staged ./dist
-git restore --source=HEAD ./dist
+git restore --source=HEAD --staged ./client/build
+git restore --source=HEAD ./client/build
 
-# Move the files from dist to the root of the repository
-mv ./dist/* ./
-rm -r dist
+# Move the files from client/build to the root of the repository
+mv ./client/build/* ./
+rm -r client
 
 # Commit the changes
 git add .
@@ -22,5 +22,5 @@ git commit -m "Deploy to GitHub Pages"
 # Push the changes to the gh-pages branch on GitHub
 git push -f origin gh-pages
 
-# Switch back to the previous branch (master in this case)
-git checkout master
+# Switch back to the previous branch
+git checkout master  # or the branch you were previously on
